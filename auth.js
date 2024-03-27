d5d1e0b432c0ed77c8f8face3c5472709259cb89
erisonn <erison.frontend@gmail.com>
2024-03-27 17:10:26 -0300

    parse bearer token

--- auth.js
+++ auth.js
@@ -6,7 +6,7 @@ dotenv.config();
 export const PRIVATE_KEY = process.env.AUTH_KEY;
 
 export const validateToken = (req, res, next) => {
-  const token = req.headers.authorization;
+  const token = req.headers.authorization?.split(' ')[1] || [' ', ' '][1];
 
   if (!token) {
     return res.status(401).send("No token provided");
